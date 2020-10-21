fc1f85ec459b5f796f114f1ad5d257b95b9c20ac
dr497 <anand.ratiney@gmail.com>
2020-10-22 00:40:23 +0800

    Updates

---
+++ precache-manifest.38238984ce6dfd559b634e67e6ada817.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b7df40dd6019beca0eec00cbdbb160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d71a1a8f7b6170efa66",
+    "url": "/static/css/2.cfe2253c.chunk.css"
+  },
+  {
+    "revision": "0b20fe94a53472129aec",
+    "url": "/static/css/main.d3e7f6f7.chunk.css"
+  },
+  {
+    "revision": "5d71a1a8f7b6170efa66",
+    "url": "/static/js/2.5d3d8708.chunk.js"
+  },
+  {
+    "revision": "2ae86ac66ca4ea4d21dad05cd152f225",
+    "url": "/static/js/2.5d3d8708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b20fe94a53472129aec",
+    "url": "/static/js/main.96eba133.chunk.js"
+  },
+  {
+    "revision": "f1d838d0d38d69dfec98",
+    "url": "/static/js/runtime-main.48866770.js"
+  },
+  {
+    "revision": "04bd3c48d022c12e4942de602ed8d589",
+    "url": "/static/media/logo-deep.04bd3c48.png"
+  },
+  {
+    "revision": "1ee5a32c4b73b2bf61c1f9abb94141c2",
+    "url": "/static/media/satoshi-btc.1ee5a32c.gif"
+  },
+  {
+    "revision": "b39177efdefff58c4cfcd441cab07310",
+    "url": "/static/media/satoshi-btc.b39177ef.JPG"
+  },
+  {
+    "revision": "ade7d87399e1a7763d4adfeeb23e6641",
+    "url": "/static/media/satoshi-closeup.ade7d873.gif"
+  },
+  {
+    "revision": "d69a92472e3ac5f4703952da30f6ba6d",
+    "url": "/static/media/satoshi-closeup.d69a9247.JPG"
+  },
+  {
+    "revision": "4c8081a583a0393468cb0c491c9cf6af",
+    "url": "/static/media/satoshi-gb.4c8081a5.JPG"
+  },
+  {
+    "revision": "fb33b42b2f451e057f760373e6a1bfc0",
+    "url": "/static/media/satoshi-gb.fb33b42b.gif"
+  },
+  {
+    "revision": "bb1a47810806ee38106fccfa27d3977c",
+    "url": "/static/media/satoshi-og.bb1a4781.gif"
+  },
+  {
+    "revision": "8d8b2974c01831e5840cf1740b7bfd72",
+    "url": "/static/media/satoshi.8d8b2974.gif"
+  },
+  {
+    "revision": "0be67a89a12ea77eab2a60ed3aa1a17a",
+    "url": "/static/media/srm.0be67a89.gif"
+  }
+]);
