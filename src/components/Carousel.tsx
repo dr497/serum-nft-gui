f1f8dbb60440faeac4cb90689f8da8fcf9108acc
dr497 <anand.ratiney@gmail.com>
2020-11-18 20:27:23 +0800

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons';
+import Spin from './Spin';
 
 const CenteredDiv = styled.div`
   display: flex;
@@ -12,6 +13,7 @@ const CenteredDiv = styled.div`
 const Carousel = ({ images }: { images: Array<any> }) => {
   const nbImages = images.length;
   const [currentImage, setCurrentImage] = useState(0);
+  const [loaded, setLoaded] = useState(false);
 
   const style = {
     icon: { fontSize: '20px', color: 'red', padding: 20 },
@@ -29,19 +31,31 @@ const Carousel = ({ images }: { images: Array<any> }) => {
           src={images[currentImage]}
           style={style.img}
           alt=""
+          onLoad={() => setLoaded(true)}
         />
       </CenteredDiv>
+      {!loaded && (
+        <CenteredDiv>
+          <Spin />
+        </CenteredDiv>
+      )}
       <CenteredDiv style={{ paddingTop: 20 }}>
         {!(currentImage <= 0) && (
           <LeftCircleOutlined
             style={style.icon}
-            onClick={() => setCurrentImage((prev) => prev - 1)}
+            onClick={() => {
+              setCurrentImage((prev) => prev - 1);
+              setLoaded(false);
+            }}
           />
         )}
         {!(currentImage >= nbImages - 1) && (
           <RightCircleOutlined
             style={style.icon}
-            onClick={() => setCurrentImage((prev) => prev + 1)}
+            onClick={() => {
+              setCurrentImage((prev) => prev + 1);
+              setLoaded(false);
+            }}
           />
         )}
       </CenteredDiv>
