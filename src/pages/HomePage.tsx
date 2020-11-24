a8e5b7b5852d7f6fd69369e8f30a1f12a59c4302
dr497 <anand.ratiney@gmail.com>
2020-11-24 16:41:28 +0800

    Update homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import NftCard from '../components/NftCard';
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import styled from 'styled-components';
-import { USE_ALL_NFTS } from '../nfts';
+import USE_NFTS, { USE_REDEEMABLE_NFTS } from '../nfts';
 import { useWindowDimensions } from '../components/utils';
+import CustomDivider from '../components/CustomDivider';
+import Space from '../components/Space';
+
+const { Title } = Typography;
 
 export const WrappedCol = styled(Col)`
   padding: 30px;
+`;
+
+const CenteredDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-bottom: 20px;
 `;
 
 const RowCard = ({ start, end, array }) => {
@@ -49,23 +61,115 @@ const HomePageRow = ({ divider, longueur, array }) => {
 };
 
 const HomePage = () => {
+  const styles = {
+    title: {
+      fontSize: '80px',
+      fontWeight: 300,
+      textAlign: 'center',
+      color: 'white',
+      marginBottom: 0,
+    } as React.CSSProperties,
+  };
   const windowDimensions = useWindowDimensions();
-  const longueur = USE_ALL_NFTS.length;
+  const longueur = USE_NFTS.length;
   return (
     <>
       {windowDimensions.width > 1600 && (
         <>
-          <HomePageRow divider={3} longueur={longueur} array={USE_ALL_NFTS} />
+          <Title level={2} style={styles.title}>
+            SHOP.
+          </Title>
+          <CenteredDiv>
+            <CustomDivider
+              fromColor={'#3333ff'}
+              toColor={'#8080ff'}
+              width={300}
+            />
+          </CenteredDiv>
+          <HomePageRow
+            divider={3}
+            longueur={longueur}
+            array={USE_REDEEMABLE_NFTS}
+          />
+          <Space height={40} />
+          <Title level={2} style={styles.title}>
+            NFT.
+          </Title>
+          <CenteredDiv>
+            <CustomDivider
+              fromColor={'#3333ff'}
+              toColor={'#8080ff'}
+              width={300}
+            />
+          </CenteredDiv>
+          <HomePageRow divider={3} longueur={longueur} array={USE_NFTS} />
         </>
       )}
       {1100 < windowDimensions.width && windowDimensions.width < 1600 && (
         <>
-          <HomePageRow divider={2} longueur={longueur} array={USE_ALL_NFTS} />
+          <Title level={2} style={styles.title}>
+            SHOP.
+          </Title>
+          <CenteredDiv>
+            <CustomDivider
+              fromColor={'#3333ff'}
+              toColor={'#8080ff'}
+              width={300}
+            />
+          </CenteredDiv>
+          <HomePageRow
+            divider={2}
+            longueur={longueur}
+            array={USE_REDEEMABLE_NFTS}
+          />
+          <Space height={40} />
+          <Title level={2} style={styles.title}>
+            NFT.
+          </Title>
+          <CenteredDiv>
+            <CustomDivider
+              fromColor={'#3333ff'}
+              toColor={'#8080ff'}
+              width={300}
+            />
+          </CenteredDiv>
+          <HomePageRow divider={2} longueur={longueur} array={USE_NFTS} />
         </>
       )}
       {windowDimensions.width < 1100 && (
         <>
-          {USE_ALL_NFTS.map((NFT, key) => {
+          <Title level={2} style={styles.title}>
+            SHOP.
+          </Title>
+          <CenteredDiv>
+            <CustomDivider
+              fromColor={'#3333ff'}
+              toColor={'#8080ff'}
+              width={300}
+            />
+          </CenteredDiv>
+          {USE_REDEEMABLE_NFTS.map((NFT, key) => {
+            return (
+              <Row style={{ padding: 20 }} align="middle" justify="center">
+                <Col flex="auto" />
+                <Col>
+                  <NftCard key={key} nft={NFT} />
+                </Col>
+                <Col flex="auto" />
+              </Row>
+            );
+          })}
+          <Title level={2} style={styles.title}>
+            NFT.
+          </Title>
+          <CenteredDiv>
+            <CustomDivider
+              fromColor={'#3333ff'}
+              toColor={'#8080ff'}
+              width={300}
+            />
+          </CenteredDiv>
+          {USE_NFTS.map((NFT, key) => {
             return (
               <Row style={{ padding: 20 }} align="middle" justify="center">
                 <Col flex="auto" />
