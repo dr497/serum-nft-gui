d910639782555be044dd4b77f7f9c13ef7367a5e
dr497 <anand.ratiney@gmail.com>
2021-01-14 00:38:10 +0800

    Socks

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
 );
 
-importScripts('/precache-manifest.b75b0f9b47874691284af4ebb91f1b5e.js');
+importScripts('/precache-manifest.62707dc5e427e17e11a4ef01dda126c9.js');
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
