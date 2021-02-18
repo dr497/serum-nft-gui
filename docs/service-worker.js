928288d7b61ba2e5ea314db6dcdd74785a2da1e9
dr497 <anand.ratiney@gmail.com>
2021-02-18 10:07:45 +0100

    New NFT

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
 );
 
-importScripts('/precache-manifest.dde9dc0c1eb856a89e143a724a09d213.js');
+importScripts('/precache-manifest.50cb142d0c005b762f8df1fead1577a9.js');
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
