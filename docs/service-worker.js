7080b3e1abe1f75f4cc1c77bd80d0afe83c95f25
dr497 <anand.ratiney@gmail.com>
2021-02-21 01:22:27 +0100

    New build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
 );
 
-importScripts('/precache-manifest.50cb142d0c005b762f8df1fead1577a9.js');
+importScripts('/precache-manifest.13d864b66adf428411b65a8fa7789217.js');
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
